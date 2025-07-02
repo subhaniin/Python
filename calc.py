f205b2c356c978be0daf7b4b37413703735103fa
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-07-02 15:08:12 +0530

    commit

--- calc.py
+++ calc.py
@@ -1,15 +1,15 @@
 def math():
 	a=int(input(" ",))
 	b=int(input(" ",))
-	print("",f"add={a+b}")
-	print("",f"sub={a-b}")
-	print("",f"mul={a*b}")
+	print(f"add={a+b}")
+	print(f"sub={a-b}")
+	print(f"mul={a*b}")
 	if b==0:
-		print(" zero div error")
+		print("zero div error")
 	else:
-		print("",f"div={int(a/b)}")
+		print(f"div={a / b}")
 while True:	
 	math()
-	choice=int(input(" enter 0 to exit"))
+	choice=int(input("enter 0 to exit"))
 	if choice==0:
 		break
