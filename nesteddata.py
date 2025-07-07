96fe8c2cce52bf884ec3b20637605c51baf9d45c
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-07-07 23:27:54 +0530

    commi

--- nesteddata.py
+++ nesteddata.py
@@ -1,9 +1,2 @@
-a=(1,2,[3,4,5,{"shaik":[3,"subhani"]}])
-keys=a[2][3]["shaik"].keys()
-print(a[2][3]["shaik"][1])
-
-
-a={"Subhani":[0,1,"subhani"]}
-
-
-#print(["subhani"])
+a=(1,2,3,4,5,6,7,8,9,0,[1,2,3,4,5,6,7,8,9,9],{"Subhani":"Shaik","Shaik":"Subhani"})
+print(a[11]["Subhani"],a[11]["Shaik"],"asdfghjk")
