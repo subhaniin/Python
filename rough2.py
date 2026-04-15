06d26781afd4c03fb13210c3b9f91fb5db35f6e5
subhaniin <70527643+subhaniin@users.noreply.github.com>
2026-04-16 00:02:43 +0530

    cc

--- rough2.py
+++ rough2.py
@@ -3,20 +3,20 @@ import random
 fake = Faker("ar_EG")
 fake = Faker("en_IN")
 
-first_names = [
-    "Mohammad", "Ahmed", "Ali", "Hassan", "Hussain", "Abdul", "Umar",
-    "Ibrahim", "Yusuf", "Bilal", "Saif", "Zaid", "Ayaan", "Arman"
-]
+#first_names = [
+#    "Mohammad", "Ahmed", "Ali", "Hassan", "Hussain", "Abdul", "Umar",
+#    "Ibrahim", "Yusuf", "Bilal", "Saif", "Zaid", "Ayaan", "Arman"
+#]
 
-last_names = [
-    "Khan", "Shaikh", "Syed", "Ansari", "Qureshi", "Pathan",
-    "Farooqi", "Rizvi", "Siddiqui", "Hashmi", "Subhani"
-]
+#last_names = [
+#    "Khan", "Shaikh", "Syed", "Ansari", "Qureshi", "Pathan",
+#    "Farooqi", "Rizvi", "Siddiqui", "Hashmi", "Subhani"
+#]
 
-for _ in range(10):
-    first = random.choice(first_names)
-    last = random.choice(last_names)
-    print(f"{first} {last}","\n")
+#for _ in range(10):
+#    first = random.choice(first_names)
+#    last = random.choice(last_names)
+#    print(f"{first} {last}","\n")
 
 for i in range(10):
     print(fake.name(),"",fake.email(),"\n")
